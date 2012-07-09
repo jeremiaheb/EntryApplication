31feae7e137b15a30f1bb60f0e55ca0fbbccc195
jeremiaheb <jblondeau@gmail.com>
2012-07-09 14:03:33 -0400

    completed first draft of substrate section, including JS to total percentages

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -30,31 +30,11 @@ $(function(){
     $("#"+targetId).show();
   });
   
-    // Hide section two until section one is completely filled out
-    /*$(".section_2").hide();
 
-    function show_or_hide_section(){
-
-      var should_display_div = new Array();
-
-      $('.section_1').find('.tab_1').each(function(){
-        // This equality test does NOT work with '0' because they time
-        // option_selects can be '0'
-        if ( $(this).val() == '' )
-          {
-            should_display_div.push( $(this) );
-          };
-      });
-
-      if ( should_display_div.length == 0 )
-        { 
-          $(".section_2").show();
-        };
-    };*/
   
 
 
-  function calculate_surface_relief( input_class_to_sum, id_to_display_total){
+  function calculate_totals( input_class_to_sum, id_to_display_total){
   
         var sum_for_display_total = new Array();
   
@@ -87,6 +67,7 @@ $(function(){
   
       };    
 
+  
     /*// Check to see if section should be shown or hidden when page loads
     show_or_hide_section();
 
@@ -97,19 +78,43 @@ $(function(){
 
 
    // Calculate total for 'surface_hard' when page loads
-    calculate_surface_relief( 'hard_relief', 'hard_relief_total' );
+    calculate_totals( 'hard_relief', 'hard_relief_total' );
 
     // Calculate total for 'surface_hard' on change
     $('.hard_relief').change(function(){
-        calculate_surface_relief( 'hard_relief', 'hard_relief_total' );
+        calculate_totals( 'hard_relief', 'hard_relief_total' );
     });
 
     // Calculate total for 'surface_soft' when page loads
-    calculate_surface_relief('soft_relief', 'soft_relief_total' );
+    calculate_totals('soft_relief', 'soft_relief_total' );
 
     // Calculate total for 'surface_soft' on change
     $('.soft_relief').change(function(){
-        calculate_surface_relief( 'soft_relief', 'soft_relief_total' );
+        calculate_totals( 'soft_relief', 'soft_relief_total' );
+    });
+    
+    // Calculate total for 'abiotic_footprint' when page loads
+    calculate_totals('abiotic_percentage', 'abiotic_percentage_total' );
+
+    // Calculate total for 'abiotic_footprint' on change
+    $('.abiotic_percentage').change(function(){
+        calculate_totals( 'abiotic_percentage', 'abiotic_percentage_total' );
+    });
+
+    // Calculate total for 'biotic_percentage_sand' when page loads
+    calculate_totals('biotic_percentage_sand', 'biotic_percentage_sand_total' );
+
+    // Calculate total for 'biotic_percentage_sand' on change
+    $('.biotic_percentage_sand').change(function(){
+        calculate_totals( 'biotic_percentage_sand', 'biotic_percentage_sand_total' );
+    });
+
+    // Calculate total for 'biotic_percentage_hardbottom' when page loads
+    calculate_totals('biotic_percentage_hardbottom', 'biotic_percentage_hardbottom_total' );
+
+    // Calculate total for 'biotic_percentage_hardbottom' on change
+    $('.biotic_percentage_hardbottom').change(function(){
+        calculate_totals( 'biotic_percentage_hardbottom', 'biotic_percentage_hardbottom_total' );
     });
 
 });

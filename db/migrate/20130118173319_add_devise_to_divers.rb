b9c2df6a43819132ea8e64f7b2c8bcfe1cbd4ee2
jeremiaheb <jblondeau@gmail.com>
2013-01-22 14:42:35 -0500

    removed columns from earlier devise to divers migration

--- db/migrate/20130118173319_add_devise_to_divers.rb
+++ db/migrate/20130118173319_add_devise_to_divers.rb
@@ -51,6 +51,17 @@ class AddDeviseToDivers < ActiveRecord::Migration
   def self.down
     # By default, we don't want to make any assumption about how to roll back a migration when your
     # model already existed. Please edit below which fields you would like to remove in this migration.
-    raise ActiveRecord::IrreversibleMigration
+    remove_index  :divers, :reset_password_token
+    remove_column :divers, :last_sign_in_ip
+    remove_column :divers, :current_sign_in_ip
+    remove_column :divers, :last_sign_in_at
+    remove_column :divers, :current_sign_in_at
+    remove_column :divers, :sign_in_count
+    remove_column :divers, :remember_created_at
+    remove_column :divers, :reset_password_sent_at
+    remove_column :divers, :reset_password_token
+    remove_column :divers, :encrypted_password
+    remove_column :divers, :email      
+
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -35,22 +35,10 @@ ActiveRecord::Schema.define(:version => 20130118185716) do
   create_table "divers", :force => true do |t|
     t.string   "diver_number"
     t.string   "diver_name"
-    t.datetime "created_at",                                :null => false
-    t.datetime "updated_at",                                :null => false
-    t.boolean  "active",                 :default => false
-    t.string   "email",                  :default => "",    :null => false
-    t.string   "encrypted_password",     :default => "",    :null => false
-    t.string   "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          :default => 0
-    t.datetime "current_sign_in_at"
-    t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip"
-    t.string   "last_sign_in_ip"
+    t.datetime "created_at",                      :null => false
+    t.datetime "updated_at",                      :null => false
+    t.boolean  "active",       :default => false
   end
-
-  add_index "divers", ["reset_password_token"], :name => "index_divers_on_reset_password_token", :unique => true
 
   create_table "habitat_types", :force => true do |t|
     t.string   "habitat_name"

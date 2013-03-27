92e6384f03474864200708860d4a1b8f2df79384
jeremiaheb <jblondeau@gmail.com>
2013-03-27 13:10:07 -0400

    changed path in seed.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,7 +11,7 @@ puts "Seeding database with data"
 
 SampleType.delete_all
 SampleType.connection.execute( 'ALTER SEQUENCE sample_types_id_seq RESTART WITH 1' )
-open("db/SupportData/SampleTypeList.csv") do |sampleTypes|
+open("#{Rails.root}/db/SupportData/SampleTypeList.csv") do |sampleTypes|
   sampleTypes.read.each_line do|sampleType|
     SampleTypeName, SampleTypeDesc = sampleType.chomp.split(",")
     SampleType.create( :sample_type_name => SampleTypeName, :sample_type_description => SampleTypeDesc )
@@ -20,7 +20,7 @@ end
 
 HabitatType.delete_all
 HabitatType.connection.execute( 'ALTER SEQUENCE habitat_types_id_seq RESTART WITH 1' )
-open("db/SupportData/HabitatList.csv") do |habitatTypes|
+open("#{Rails.root}/db/SupportData/HabitatList.csv") do |habitatTypes|
   habitatTypes.read.each_line do|habitatType|
     HabName, HabDesc = habitatType.chomp.split(",")
     HabitatType.create( :habitat_name => HabName, :habitat_description => HabDesc )
@@ -29,7 +29,7 @@ end
 
 Diver.delete_all
 Diver.connection.execute( 'ALTER SEQUENCE divers_id_seq RESTART WITH 1' )
-open("db/SupportData/DiverList.csv") do |divers|
+open("#{Rails.root}/db/SupportData/DiverList.csv") do |divers|
   divers.read.each_line do|diver|
     DiverNumber, DiverName = diver.chomp.split(",")
     Diver.create( :diver_number => DiverNumber, :diver_name => DiverName )
@@ -38,7 +38,7 @@ end
 
 Animal.delete_all
 Animal.connection.execute( 'ALTER SEQUENCE animals_id_seq RESTART WITH 1' )
-open("db/SupportData/SpeciesList.csv") do |animals|
+open("#{Rails.root}/db/SupportData/SpeciesList.csv") do |animals|
   animals.read.each_line do |animal|
     SppCode, ScientificName, CommonName, MaxSize, MinSize, MaxNumber = animal.chomp.split(",")
     Animal.create( :species_code => SppCode, :scientific_name => ScientificName, :common_name => CommonName, :max_size => MaxSize, :min_size => MinSize, :max_number => MaxNumber )

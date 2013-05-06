40641d4d6f39a0ce979b0689512707ae63a6ecf7
jeremiaheb <jblondeau@gmail.com>
2013-05-06 13:17:44 -0400

    Configure before heroku deployment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,20 @@
 EntryApplication::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
 
+  config.action_mailer.default_url_options = { :host => 'entryapplication.herokuapp.com' }
+    config.action_mailer.delivery_method = :smtp
+    config.action_mailer.perform_deliveries = true
+    config.action_mailer.raise_delivery_errors = false
+    config.action_mailer.default :charset => "utf-8"
+    config.action_mailer.smtp_settings = {
+      address: "smtp.gmail.com",
+      port: 587,
+      domain: "entryapplication.herokuapp.com",
+      authentication: "plain",
+      enable_starttls_auto: true,
+      user_name: ENV["GMAIL_USERNAME"],
+      password: ENV["GMAIL_PASSWORD"]
+    } 
   # Code is not reloaded between requests
   config.cache_classes = true
 

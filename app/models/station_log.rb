5e6042a231368782ed9ff1717e4a5b8b8891121c
jeremiaheb <jblondeau@gmail.com>
2013-07-26 09:20:51 -0400

    setup fields for boatlog form

--- app/models/station_log.rb
+++ app/models/station_log.rb
@@ -2,6 +2,6 @@ class StationLog < ActiveRecord::Base
   
   belongs_to :boat_log
   has_many :rep_logs, :dependent => :destroy
-  accepts_nested_attributes_for :rep_logs, :allow_destroy => true
+  accepts_nested_attributes_for :rep_logs, :reject_if => lambda {  |a| a[:replicate].blank? }, :allow_destroy => true
 
 end

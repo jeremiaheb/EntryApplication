5e41e9014cdcba597971c7b8ec35b13be5fd733d
jeremiaheb <jblondeau@gmail.com>
2013-08-02 09:36:36 -0400

    Add and remove station functionality

--- app/models/boat_log.rb
+++ app/models/boat_log.rb
@@ -2,7 +2,7 @@ class BoatLog < ActiveRecord::Base
 
   has_many :station_logs, :dependent => :destroy
   has_many :rep_logs, :through => :station_logs
-  accepts_nested_attributes_for :station_logs, :allow_destroy => true
+  accepts_nested_attributes_for :station_logs, :reject_if => lambda {  |a| a[:stn_number].blank? }, :allow_destroy => true
 
 
   validates :primary_sample_unit, :presence => true
--- app/models/station_log.rb
+++ app/models/station_log.rb
@@ -5,51 +5,5 @@ class StationLog < ActiveRecord::Base
   accepts_nested_attributes_for :rep_logs, :reject_if => lambda {  |a| a[:replicate].blank? }, :allow_destroy => true
 
 
-  validates :stn_number, :presence => true
-
-  #validate :stations_less_than_50m
-
-  EARTH_RADIUS = 6371000 # Earth's radius in meters
-
-  # convert degrees to radians
-  def convDegRad(value)
-    unless value.nil? or value == 0
-          value = (value/180) * Math::PI
-    end
-  return value
-  end
-
-  def coorDist
-
-    stn1_coordinates = [ self.where(stn_number: 1).first.latitude, self.where(stn_number: 1).first.longitude ]
-    stn2_coordinates = [ self.where(stn_number: 2).first.latitude, self.where(stn_number: 2).first.longitude ]
-    
-
-    deltaLat = (stn2_coordinates[0] - stn1_coordinates[0])
-    deltaLon = (stn2_coordinates[1] - stn1_coordinates[1])
-    deltaLat = convDegRad(deltaLat)
-    deltaLon = convDegRad(deltaLon)
-
-    # Calculate square of half the chord length between latitude and longitude
-    a = Math.sin(deltaLat/2) * Math.sin(deltaLat/2) +
-        Math.cos((stn1_coordinates[0]/180 * Math::PI)) * Math.cos((stn2_coordinates[0]/180 * Math::PI)) *
-        Math.sin(deltaLon/2) * Math.sin(deltaLon/2); 
-
-    # Calculate the angular distance in radians
-    c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a))
-
-    distance = EARTH_RADIUS * c
-
-    return distance
-
-  end
-
-  private
-
-  def stations_less_than_50m
-    if coorDist > 50
-      self.errors.add :comments, ' Stations are greater than 50m apart '
-    end
-  end
 
 end

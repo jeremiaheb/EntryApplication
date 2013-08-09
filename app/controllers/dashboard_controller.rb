735ffebfe0cce367aeeffaa311f5dee1194883df
jeremiaheb <jblondeau@gmail.com>
2013-08-09 10:55:06 -0400

    dashboard model methods to instance methods

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,10 +1,9 @@
 class DashboardController < ApplicationController
-
+ 
   before_filter :authenticate_diver!
   authorize_resource
-
+ 
  def show
-   @data_by_divers = Dashboard.divers(current_diver)
+   @data_by_divers = Dashboard.new.divers(current_diver)
  end
-
 end

f35ad0a8e738dc5b1e6a9d27b9cb3dd12dbb6968
jeremiaheb <jblondeau@gmail.com>
2014-04-03 12:12:56 -0400

    url_override in applicationController

--- config/initializers/prefix_RVC_Data_Entry_to_url.rb
+++ config/initializers/prefix_RVC_Data_Entry_to_url.rb
@@ -18,12 +18,3 @@ ActionView::RoutingUrlFor.module_eval do
   end
 end
 
-ActionController::UrlFor.module_eval do
-  if Rails.env.production?
-    def url_for(options = nil)
-      results = super(options)
-      results.insert(0, "/RVC_Data_Entry") unless results.match /^\/RVC_Data_Entry/
-        results
-    end
-  end
-end

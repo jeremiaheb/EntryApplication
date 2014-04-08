c596b7f5ae4a56ce84cd6334cb4b06edf1454781
jeremiaheb <jblondeau@gmail.com>
2014-04-08 09:56:01 -0400

    comment out subURI prefix for heroku push

--- config/initializers/prefix_RVC_Data_Entry_to_url.rb
+++ config/initializers/prefix_RVC_Data_Entry_to_url.rb
@@ -8,15 +8,15 @@
   #end
 #end
 
-ActionView::RoutingUrlFor.module_eval do
-  if Rails.env.production?
-    def url_for(options = nil)
-      results = super(options)
-      results.insert(0, "/RVC_Data_Entry") unless results.match /^\/RVC_Data_Entry/
-        results
-    end
-  end
-end
+#ActionView::RoutingUrlFor.module_eval do
+  #if Rails.env.production?
+    #def url_for(options = nil)
+      #results = super(options)
+      #results.insert(0, "/RVC_Data_Entry") unless results.match /^\/RVC_Data_Entry/
+        #results
+    #end
+  #end
+#end
 
 #ActionView::RoutingUrlFor.module_eval do
     #def url_for(options = nil)

533ab5492d6c0a025345f20f324e0206450a8a65
jeremiaheb <jblondeau@gmail.com>
2014-05-06 13:36:36 -0400

    comment out seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -27,24 +27,24 @@ puts "Seeding database with data"
   #end
 #end
 
-Diver.delete_all
-Diver.connection.execute( 'ALTER SEQUENCE divers_id_seq RESTART WITH 1' )
-open("#{Rails.root}/db/SupportData/DeviseDivers.csv") do |divers|
-  divers.read.each_line do|diver|
-    DiverNumber, DiverName, UserName, Email, Password, Active, Role = diver.chomp.split(",")
-    Diver.create( :diver_number => DiverNumber, :diver_name => DiverName, :username => UserName, :email => Email, :password => Password, :active => Active, :role => Role )
-    puts "successfully created #{DiverName}"
-  end
-end
+#Diver.delete_all
+#Diver.connection.execute( 'ALTER SEQUENCE divers_id_seq RESTART WITH 1' )
+#open("#{Rails.root}/db/SupportData/DeviseDivers.csv") do |divers|
+  #divers.read.each_line do|diver|
+    #DiverNumber, DiverName, UserName, Email, Password, Active, Role = diver.chomp.split(",")
+    #Diver.create( :diver_number => DiverNumber, :diver_name => DiverName, :username => UserName, :email => Email, :password => Password, :active => Active, :role => Role )
+    #puts "successfully created #{DiverName}"
+  #end
+#end
 
-Animal.delete_all
-Animal.connection.execute( 'ALTER SEQUENCE animals_id_seq RESTART WITH 1' )
-open("#{Rails.root}/db/SupportData/RVC_Species_List.csv") do |animals|
-  animals.read.each_line do |animal|
-    SppCode, ScientificName, CommonName, MaxSize, MinSize, MaxNumber = animal.chomp.split(",")
-    Animal.create( :species_code => SppCode, :scientific_name => ScientificName, :common_name => CommonName, :max_size => MaxSize, :min_size => MinSize, :max_number => MaxNumber )
-  end
-end
+#Animal.delete_all
+#Animal.connection.execute( 'ALTER SEQUENCE animals_id_seq RESTART WITH 1' )
+#open("#{Rails.root}/db/SupportData/RVC_Species_List.csv") do |animals|
+  #animals.read.each_line do |animal|
+    #SppCode, ScientificName, CommonName, MaxSize, MinSize, MaxNumber = animal.chomp.split(",")
+    #Animal.create( :species_code => SppCode, :scientific_name => ScientificName, :common_name => CommonName, :max_size => MaxSize, :min_size => MinSize, :max_number => MaxNumber )
+  #end
+#end
 
 #Coral.delete_all
 #Coral.connection.execute( 'ALTER SEQUENCE corals_id_seq RESTART WITH 1' )

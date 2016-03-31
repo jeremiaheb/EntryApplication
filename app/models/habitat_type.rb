90af43d6b7d04c0f622e7002f61585da30399178
jeremiaheb <jblondeau@gmail.com>
2016-03-31 10:18:47 -0400

    added scope for caribbean habitats, updated hab list and seed

--- app/models/habitat_type.rb
+++ app/models/habitat_type.rb
@@ -4,6 +4,7 @@ class HabitatType < ActiveRecord::Base
   has_many :benthic_covers
 
   scope       :atlantic_habitats,      lambda { where(:region => "Atlantic") }
+  scope       :caribbean_habitats,      lambda { where(:region => "Caribbean") }
   
   validates :habitat_name, :presence => true
   validates :habitat_description, :presence => true

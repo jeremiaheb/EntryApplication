00fc257e5836e517b0482989df2f712fc11a10a7
jeremiaheb <jblondeau@gmail.com>
2016-07-27 09:45:16 -0400

    fixed demo and LPI elements moving to bottom after edits

--- app/models/point_intercept.rb
+++ app/models/point_intercept.rb
@@ -5,6 +5,7 @@ class PointIntercept < ActiveRecord::Base
   belongs_to  :benthic_cover
   belongs_to  :cover_cat
 
+  default_scope -> { order("id ASC") }
   validates   :cover_cat_id,    :presence => true
 
   def cover_total

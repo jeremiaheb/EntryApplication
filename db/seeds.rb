e725203d6487fb0a272747268c465fd1b432e2c8
jeremiaheb <jblondeau@gmail.com>
2017-05-18 08:32:34 -0400

    add .swp to gitignore, unncomment seed scripts, Gemfile.lock change

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,23 +9,23 @@
 
 puts "Seeding database with data"
 
-#SampleType.delete_all
-#SampleType.connection.execute( 'ALTER SEQUENCE sample_types_id_seq RESTART WITH 1' )
-#open("#{Rails.root}/db/SupportData/SampleTypeList.csv") do |sampleTypes|
-  #sampleTypes.read.each_line do|sampleType|
-    #SampleTypeName, SampleTypeDesc = sampleType.chomp.split(",")
-    #SampleType.create( :sample_type_name => SampleTypeName, :sample_type_description => SampleTypeDesc )
-  #end
-#end
+SampleType.delete_all
+SampleType.connection.execute( 'ALTER SEQUENCE sample_types_id_seq RESTART WITH 1' )
+open("#{Rails.root}/db/SupportData/SampleTypeList.csv") do |sampleTypes|
+  sampleTypes.read.each_line do|sampleType|
+    SampleTypeName, SampleTypeDesc = sampleType.chomp.split(",")
+    SampleType.create( :sample_type_name => SampleTypeName, :sample_type_description => SampleTypeDesc )
+  end
+end
 
-#HabitatType.delete_all
-#HabitatType.connection.execute( 'ALTER SEQUENCE habitat_types_id_seq RESTART WITH 1' )
-#open("#{Rails.root}/db/SupportData/HabitatList.csv") do |habitatTypes|
-  #habitatTypes.read.each_line do|habitatType|
-    #HabName, HabDesc, HabReg = habitatType.chomp.split(",")
-    #HabitatType.create( :habitat_name => HabName, :habitat_description => HabDesc, :region => HabReg )
-  #end
-#end
+HabitatType.delete_all
+HabitatType.connection.execute( 'ALTER SEQUENCE habitat_types_id_seq RESTART WITH 1' )
+open("#{Rails.root}/db/SupportData/HabitatList.csv") do |habitatTypes|
+  habitatTypes.read.each_line do|habitatType|
+    HabName, HabDesc, HabReg = habitatType.chomp.split(",")
+    HabitatType.create( :habitat_name => HabName, :habitat_description => HabDesc, :region => HabReg )
+  end
+end
 
 Diver.delete_all
 Diver.connection.execute( 'ALTER SEQUENCE divers_id_seq RESTART WITH 1' )

ad1666bebb558cbcb78e217dbc55e7ba8d82aba6
jeremiaheb <jblondeau@gmail.com>
2017-05-19 22:26:29 -0400

    edit devise sanitizer to .permit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,8 +14,10 @@ class ApplicationController < ActionController::Base
     protected
 
     def configure_permitted_parameters
-      devise_parameter_sanitizer.for(:sign_up) << :firstname << :lastname << :email
-      devise_parameter_sanitizer.for(:account_update) << :firstname << :lastname << :email
+      devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit({ roles: [] }, :email, :password, :password_confirmation, :username) }
+      #devise_parameter_sanitizer.permit(:sign_up) << :firstname << :lastname << :email
+      #devise_parameter_sanitizer.permit(:account_update) << :firstname << :lastname << :email
+      devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:firstname, :lastname, :email)  }
     end
 
 end

ae7e719888e7434fe665511fee1f4680f976552a
jeremiaheb <jblondeau@gmail.com>
2017-05-20 17:47:58 -0400

    added datepicker-bootstrap to fix date format issue

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,6 +15,7 @@
 //= require jquery_ujs
 //= require dataTables/jquery.dataTables
 //= require dataTables/bootstrap/3/jquery.dataTables.bootstrap
+//= require bootstrap-datepicker
 //= require select2
 //= require jquery.validate
 //= require twitter/bootstrap

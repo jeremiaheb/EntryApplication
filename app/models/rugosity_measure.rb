f6b13c7c2d3abbdc1fdfff79dcf951965c655ff2
jeremiaheb <jblondeau@gmail.com>
2019-04-08 11:59:04 -0400

    add new rugosity fields to model validations

--- app/models/rugosity_measure.rb
+++ app/models/rugosity_measure.rb
@@ -6,12 +6,21 @@ belongs_to :benthic_cover
   validates :min_depth,               :presence => true
   validates :max_depth,               :presence => true
   validates :max_vert_height,         :presence => true
-  validates :cnt_less_than_20,        :presence => true
-  validates :cnt_20_less_than_50,     :presence => true
-  validates :cnt_50_less_than_100,    :presence => true
-  validates :cnt_100_less_than_150,   :presence => true
-  validates :cnt_150_less_than_200,   :presence => true
-  validates :cnt_greater_than_200,    :presence => true
+  validates :meter_mark_1,            :presence => true
+  validates :meter_mark_2,            :presence => true
+  validates :meter_mark_3,            :presence => true
+  validates :meter_mark_4,            :presence => true
+  validates :meter_mark_5,            :presence => true
+  validates :meter_mark_6,            :presence => true
+  validates :meter_mark_7,            :presence => true
+  validates :meter_mark_8,            :presence => true
+  validates :meter_mark_9,            :presence => true
+  validates :meter_mark_10,            :presence => true
+  validates :meter_mark_11,            :presence => true
+  validates :meter_mark_12,            :presence => true
+  validates :meter_mark_13,            :presence => true
+  validates :meter_mark_14,            :presence => true
+  validates :meter_mark_15,            :presence => true
 
 
   def category_sum

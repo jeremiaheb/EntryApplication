2f1d6136b211ab85a64f92b5cbbb95d24b52e579
jeremiaheb <jblondeau@gmail.com>
2019-04-17 14:10:04 -0400

    update seeds to correct file

--- db/seeds.rb
+++ db/seeds.rb
@@ -29,7 +29,7 @@ puts "Seeding database with data"
 
 Diver.delete_all
 Diver.connection.execute( 'ALTER SEQUENCE divers_id_seq RESTART WITH 1' )
-open("#{Rails.root}/db/SupportData/ncrmp_diverlist_2018.csv") do |divers|
+open("#{Rails.root}/db/SupportData/ncrmp_diverlist_2019.csv") do |divers|
   divers.read.each_line do|diver|
     DiverNumber, DiverName, UserName, Email, Password, Active, Role = diver.chomp.split(",")
     Diver.create( :diver_number => DiverNumber, :diver_name => DiverName, :username => UserName, :email => Email, :password => Password, :active => Active, :role => Role )
@@ -39,7 +39,7 @@ end
 
 Animal.delete_all
 Animal.connection.execute( 'ALTER SEQUENCE animals_id_seq RESTART WITH 1' )
-open("#{Rails.root}/db/SupportData/FishSpecies_April2016.csv") do |animals|
+open("#{Rails.root}/db/SupportData/Fish_SpeciesList_2019.csv") do |animals|
   animals.read.each_line do |animal|
     SppCode, ScientificName, CommonName, MinSize, MaxSize, MaxNumber = animal.chomp.split(",")
     Animal.create( :species_code => SppCode, :scientific_name => ScientificName, :common_name => CommonName, :min_size => MinSize, :max_size => MaxSize, :max_number => MaxNumber )
@@ -48,7 +48,7 @@ end
 
 Coral.delete_all
 Coral.connection.execute( 'ALTER SEQUENCE corals_id_seq RESTART WITH 1' )
-open("#{Rails.root}/db/SupportData/CoralSpecies_April2016.csv") do |corals|
+open("#{Rails.root}/db/SupportData/CoralSpecies_April2019.csv") do |corals|
   corals.read.each_line do |coral|
     Code, ScientificName, CommonName, Category = coral.chomp.split(",")
     Coral.create( :scientific_name => ScientificName, :code => Code)
@@ -58,7 +58,7 @@ end
 
 CoverCat.delete_all
 CoverCat.connection.execute( 'ALTER SEQUENCE cover_cats_id_seq RESTART WITH 1' )
-open("#{Rails.root}/db/SupportData/LPISpeciesList_April2016.csv") do |corals|
+open("#{Rails.root}/db/SupportData/LPISpeciesList_April2019.csv") do |corals|
   corals.read.each_line do |coral|
     Code, ScientificName, CommonName = coral.chomp.split(",")
     CoverCat.create( :name => ScientificName, :code => Code, :common => CommonName)

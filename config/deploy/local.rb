ed733a4ea09b60c0564fb358fb641abe1cd31891
Andy Lindeman <andy.lindeman@noaa.gov>
2025-03-24 08:43:24 -0400

    Rename vagrant deploy target to local

---
+++ config/deploy/local.rb
@@ -0,0 +1,4 @@
+set :stage, :vagrant
+set :rails_env, "production"
+
+server "127.0.0.1", user: "entryapplication"
--- config/deploy/vagrant.rb
+++
@@ -1,4 +0,0 @@
-set :stage, :vagrant
-set :rails_env, "production"
-
-server "127.0.0.1", user: "entryapplication", port: "2222"

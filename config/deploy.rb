d6816f1ad39b336d96a60bdabd0608289ec39ef3
Andy Lindeman <andy.lindeman@noaa.gov>
2025-03-24 16:32:23 +0000

    Link encryption keys to shared/ directory

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,7 +4,7 @@ set :application, "EntryApplication"
 set :repo_url, "git@github.com:jeremiaheb/EntryApplication.git"
 set :branch, ENV["BRANCH"] if ENV["BRANCH"]
 set :deploy_to, "/var/www/apps/EntryApplication"
-set :linked_dirs, %w[log tmp/pids tmp/cache tmp/sockets vendor/bundle]
+set :linked_dirs, %w[log tmp/pids tmp/cache tmp/sockets vendor/bundle config/master.key config/credentials/production.key]
 
 set :ssh_options, forward_agent: true
 

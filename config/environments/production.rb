569a25facfc00e62f23c86ae40f2993ad875da70
Andy Lindeman <andy.lindeman@noaa.gov>
2025-04-22 17:01:43 -0400

    Send email, maybe

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -75,6 +75,8 @@ Rails.application.configure do
   # config.active_job.queue_adapter = :resque
   # config.active_job.queue_name_prefix = "railsdiff_production"
 
+  config.action_mailer.delivery_method = :sendmail
+  config.action_mailer.perform_deliveries = true
   config.action_mailer.perform_caching = false
 
   # Ignore bad email addresses and do not raise email delivery errors.

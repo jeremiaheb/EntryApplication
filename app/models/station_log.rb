2962a54d20e66b44f29bc31cd81034e0174ef397
Andy Lindeman <andy.lindeman@noaa.gov>
2025-05-09 13:13:17 -0400

    Input lat/long in positive only (assume N/W)
    % Conflicts:
    %	app/models/station_log.rb

--- app/models/station_log.rb
+++ app/models/station_log.rb
@@ -1,14 +1,31 @@
 class StationLog < ActiveRecord::Base
   belongs_to :boat_log
   has_many :rep_logs, :dependent => :destroy
-  accepts_nested_attributes_for :rep_logs, :reject_if => lambda {  |a| a[:replicate].blank? }, :allow_destroy => true
+  accepts_nested_attributes_for :rep_logs, :allow_destroy => true
 
   validates :stn_number,        :presence => true, :length => { :is => 1 }, :numericality => { :only_integer => true }
   validates :time,              :presence => true
-  validates :latitude,          :presence => true, :numericality => true
-  validates :longitude,         :presence => true, :numericality => true
+  validates :latitude,          :presence => true, :numericality => { greater_than: 0, message: "must be positive (northern hemisphere)" }
+  validates :longitude,         :presence => true, :numericality => { less_than: 0, message: "must be negative (western hemisphere)" }
 
   def psu
     return self.boat_log.primary_sample_unit
+  end
+
+  def latitude_in_northern_degrees
+    latitude
+  end
+
+  def latitude_in_northern_degrees=(northern_degrees)
+    self.latitude = northern_degrees.presence&.to_f
+  end
+
+  def longitude_in_western_degrees
+    -longitude if longitude
+  end
+
+  def longitude_in_western_degrees=(western_degrees)
+    # 123.send(:-@) is equivalent to -123 (i.e., it negates the number)
+    self.longitude = western_degrees.presence&.to_f&.send(:-@)
   end
 end

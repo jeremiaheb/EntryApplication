4a23de41e823843e7fbb99a8de591cb2260911ff
Andy Lindeman <andy.lindeman@noaa.gov>
2025-06-23 15:29:15 -0400

    Enumerate values for ESA corals
    The proofing report will have the human friendly names while the underlying database continues storing the integer values

--- app/models/presence_belt.rb
+++ app/models/presence_belt.rb
@@ -1,8 +1,15 @@
 class PresenceBelt < ActiveRecord::Base
+  ENUM_VALUES = {
+    "Absent"           => 0,
+    "Present_Transect" => 1, 
+    "Present_Site"     => 2,
+    "Did Not Look"     => 3 
+  }
+
   attr_protected []
 
   belongs_to :benthic_cover
- 
+
   validates :a_palmata,         :presence => true
   validates :a_cervicornis,     :presence => true
   validates :d_cylindrus,       :presence => true
@@ -10,5 +17,14 @@ class PresenceBelt < ActiveRecord::Base
   validates :m_annularis,       :presence => true
   validates :m_franksi,         :presence => true
   validates :m_faveolata,       :presence => true
-
+ 
+  enum a_palmata:  ENUM_VALUES,
+    a_cervicornis: ENUM_VALUES,
+    d_cylindrus:   ENUM_VALUES,
+    m_ferox:       ENUM_VALUES,
+    m_annularis:   ENUM_VALUES,
+    m_franksi:     ENUM_VALUES,
+    m_faveolata:   ENUM_VALUES,
+    _prefix:       true,
+    _suffix:       false
 end

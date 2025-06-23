399c37791d968a505bfcf3953a1ba8f5c1a5151a
Andy Lindeman <andy.lindeman@noaa.gov>
2025-06-23 15:29:15 -0400

    Enumerate values for ESA corals
    The proofing report will have the human friendly names while the underlying database continues storing the integer values
    
    % Conflicts:
    %	app/models/presence_belt.rb

--- app/models/presence_belt.rb
+++ app/models/presence_belt.rb
@@ -1,11 +1,39 @@
 class PresenceBelt < ActiveRecord::Base
+  ENUM_VALUES = {
+    "Absent"           => 0,
+    "Present_Transect" => 1, 
+    "Present_Site"     => 2,
+    "Did Not Look"     => 3 
+  }
+
   belongs_to :benthic_cover
- 
-  validates :a_palmata,         :presence => true
-  validates :a_cervicornis,     :presence => true
-  validates :d_cylindrus,       :presence => true
-  validates :m_ferox,           :presence => true
-  validates :m_annularis,       :presence => true
-  validates :m_franksi,         :presence => true
-  validates :m_faveolata,       :presence => true
+
+  enum :a_palmata, ENUM_VALUES,
+    validate: true,
+    scopes: false,
+    instance_methods: false
+  enum :a_cervicornis, ENUM_VALUES,
+    validate: true,
+    scopes: false,
+    instance_methods: false
+  enum :d_cylindrus, ENUM_VALUES,
+    validate: true,
+    scopes: false,
+    instance_methods: false
+  enum :m_ferox, ENUM_VALUES,
+    validate: true,
+    scopes: false,
+    instance_methods: false
+  enum :m_annularis, ENUM_VALUES,
+    validate: true,
+    scopes: false,
+    instance_methods: false
+  enum :m_franksi, ENUM_VALUES,
+    validate: true,
+    scopes: false,
+    instance_methods: false
+  enum :m_faveolata, ENUM_VALUES,
+    validate: true,
+    scopes: false,
+    instance_methods: false
 end

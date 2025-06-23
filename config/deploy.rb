85ddf346601a3aa1969ade57bd6fa6682ce618c7
Andy Lindeman <andy.lindeman@noaa.gov>
2025-06-23 15:39:05 -0400

    Ask for branch name
    % Conflicts:
    %	config/deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@ lock "3.19.2"
 
 set :application, "EntryApplication"
 set :repo_url, "git@github.com:jeremiaheb/EntryApplication.git"
-set :branch, ENV["BRANCH"] if ENV["BRANCH"]
+ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }
 set :deploy_to, "/var/www/apps/EntryApplication"
 set :linked_dirs, %w[log tmp/pids tmp/cache tmp/sockets vendor/bundle]
 set :linked_files, %w[config/master.key config/credentials/production.key]

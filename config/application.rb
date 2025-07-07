f5b0952588f7d6643d65f970e7acc6732bc0146a
Andy Lindeman <andy.lindeman@noaa.gov>
2025-07-07 12:23:42 -0400

    Autoload from lib/

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,9 @@ module EntryApplication
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
 
+    # Load files from lib/
+    config.autoload_paths += [config.root.join("lib").to_s]
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading

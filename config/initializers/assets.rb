cfed33119c34ec3900c897ee8fdf0740eed7a7fa
Andy Lindeman <andy.lindeman@noaa.gov>
2025-07-17 10:19:53 -0400

    Re-add missing asset paths

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -5,6 +5,12 @@ Rails.application.config.assets.version = "1.0"
 
 # Add additional assets to the asset load path.
 # Rails.application.config.assets.paths << Emoji.images_path
+Rails.application.config.assets.paths << Rails.root.join("node_modules")
+Rails.application.config.assets.paths << Rails.root.join("node_modules", "@uswds", "uswds", "dist", "img")
+Rails.application.config.assets.paths << Rails.root.join("node_modules", "@uswds", "uswds", "packages")
+Rails.application.config.assets.paths << Rails.root.join("vendor", "bootstrap")
+Rails.application.config.assets.paths << Rails.root.join("vendor", "jquery.timeentry.package-2.0.1")
+Rails.application.config.assets.paths << Rails.root.join("vendor", "select2-3.2.0")
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets

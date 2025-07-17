85fd4e23dd01c65ac7a74ea1d2c240a6914c6430
Andy Lindeman <andy.lindeman@noaa.gov>
2025-07-17 10:14:17 -0400

    bin/rails app:update

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,14 +1,12 @@
 # Be sure to restart your server when you modify this file.
 
 # Version of your assets, change this if you want to expire all your assets.
-Rails.application.config.assets.version = "2.0"
+Rails.application.config.assets.version = "1.0"
 
 # Add additional assets to the asset load path.
 # Rails.application.config.assets.paths << Emoji.images_path
-# Add Yarn node_modules folder to the asset load path.
-Rails.application.config.assets.paths << Rails.root.join("node_modules")
-Rails.application.config.assets.paths << Rails.root.join("node_modules", "@uswds", "uswds", "dist", "img")
-Rails.application.config.assets.paths << Rails.root.join("node_modules", "@uswds", "uswds", "packages")
-Rails.application.config.assets.paths << Rails.root.join("vendor", "bootstrap")
-Rails.application.config.assets.paths << Rails.root.join("vendor", "jquery.timeentry.package-2.0.1")
-Rails.application.config.assets.paths << Rails.root.join("vendor", "select2-3.2.0")
+
+# Precompile additional assets.
+# application.js, application.css, and all non-JS/CSS in the app/assets
+# folder are already added.
+# Rails.application.config.assets.precompile += %w( admin.js admin.css )

d204bae85cb02640a131169b567f14734b3239a4
Andy Lindeman <andy.lindeman@noaa.gov>
2025-08-06 12:29:14 -0400

    ApplicationMailer does not exist yet

--- config/initializers/solid_errors.rb
+++ config/initializers/solid_errors.rb
@@ -1,4 +1,4 @@
 Rails.application.config.solid_errors.send_emails = Rails.env.production?
-Rails.application.config.solid_errors.email_from = ApplicationMailer.default[:from]
+Rails.application.config.solid_errors.email_from = "NCRMP Data Entry <jeremiah.blondeau+entryapplication@noaa.gov>"
 Rails.application.config.solid_errors.email_to = "andy.lindeman@noaa.gov"
 Rails.application.config.solid_errors.destroy_after = 90.days

5950852398bc102d2f7a076d9d84ae3c6dcd5b4b
Andy Lindeman <andy.lindeman@noaa.gov>
2025-08-12 14:32:54 -0400

    Implement USWDS for benthic_covers/index

--- app/controllers/benthic_covers_controller.rb
+++ app/controllers/benthic_covers_controller.rb
@@ -2,6 +2,8 @@ class BenthicCoversController < ApplicationController
   before_action :authenticate_diver!
   load_and_authorize_resource
 
+  layout "application-uswds"
+
   # GET /benthic_covers
   def index
     if current_diver.admin?

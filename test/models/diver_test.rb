1f1d2040c7907fc1fe3de61186ef53993556b7ba
Andy Lindeman <andy.lindeman@noaa.gov>
2025-08-21 14:30:03 -0400

    Fix N+1 queries for coral demographics report

--- test/models/diver_test.rb
+++ test/models/diver_test.rb
@@ -5,15 +5,4 @@ class DiverTest < ActiveSupport::TestCase
     diver = FactoryBot.create(:diver)
     assert diver.valid?
   end
-
-  test "#diver_proofing_coral_demo returns a diver's coral demographics" do
-    diver = FactoryBot.create(:diver)
-    boatlog_manager = FactoryBot.create(:boatlog_manager)
-    coral_demographic1 = FactoryBot.create(:coral_demographic, boatlog_manager: boatlog_manager, diver: diver)
-    coral_demographic2 = FactoryBot.create(:coral_demographic, boatlog_manager: boatlog_manager, diver: diver)
-    coral_demographic3 = FactoryBot.create(:coral_demographic, diver: diver)
-    coral_demographic4 = FactoryBot.create(:coral_demographic)
-
-    assert_equal [coral_demographic1, coral_demographic2, coral_demographic3].sort, diver.diver_proofing_coral_demo
-  end
 end

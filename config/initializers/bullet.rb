456ed3ecb0739066d0f07f0382b25a7fd45f1db4
Andy Lindeman <andy.lindeman@noaa.gov>
2025-11-20 11:45:47 -0500

    Allow bullet to be optionally disabled in development

--- config/initializers/bullet.rb
+++ config/initializers/bullet.rb
@@ -2,13 +2,13 @@ Rails.application.config.after_initialize do
   # Report on possible N+1 queries in development and test environments
   case Rails.env
   when "development"
-    Bullet.enable        = true
+    Bullet.enable        = !ENV.fetch("BULLET_DISABLE", false)
     Bullet.bullet_logger = true
     Bullet.console       = true
     Bullet.rails_logger  = true
     Bullet.add_footer    = true
   when "test"
-    Bullet.enable        = true
+    Bullet.enable        = !ENV.fetch("BULLET_DISABLE", false)
     Bullet.bullet_logger = true
   end
 end

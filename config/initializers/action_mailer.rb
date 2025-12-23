4b572bbdd20d99104371cb50f5078e8fdba195a2
Andy Lindeman <andy.lindeman@noaa.gov>
2025-12-23 10:58:24 -0500

    Set script_name for relative paths

--- config/initializers/action_mailer.rb
+++ config/initializers/action_mailer.rb
@@ -7,9 +7,15 @@ when "development", "test"
   Rails.application.config.action_mailer.raise_delivery_errors = false
 when "production"
   if GoogleCloudDetector.running_on_google_cloud?
-    Rails.application.config.action_mailer.default_url_options = { host: "ncrmp-data-entry.fisheries.noaa.gov" }
+    Rails.application.config.action_mailer.default_url_options = {
+      host: "ncrmp-data-entry.fisheries.noaa.gov",
+      script_name: "/ncrmp_data_entry",
+    }
   else
-    Rails.application.config.action_mailer.default_url_options = { host: "grunt.sefsc.noaa.gov" }
+    Rails.application.config.action_mailer.default_url_options = {
+      host: "grunt.sefsc.noaa.gov",
+      script_name: "/caribbean_data_entry",
+    }
   end
 
   Rails.application.config.action_mailer.delivery_method = :sendmail

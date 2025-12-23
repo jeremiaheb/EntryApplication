2d5241c27af2d93470ae11f164ab2490e2eeba13
Andy Lindeman <andy.lindeman@noaa.gov>
2025-12-23 11:24:43 -0500

    Use default email_from from ActionMailer

--- config/initializers/solid_errors.rb
+++ config/initializers/solid_errors.rb
@@ -1,4 +1,3 @@
 Rails.application.config.solid_errors.send_emails = Rails.env.production?
-Rails.application.config.solid_errors.email_from = "NCRMP Data Entry <jeremiah.blondeau+entryapplication@noaa.gov>"
 Rails.application.config.solid_errors.email_to = "andy.lindeman@noaa.gov, jeremiah.blondeau@noaa.gov"
 Rails.application.config.solid_errors.destroy_after = 90.days

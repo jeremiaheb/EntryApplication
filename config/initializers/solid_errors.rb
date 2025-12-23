d36654c77935eb0cc3a6ac624965da786585d469
Andy Lindeman <andy.lindeman@noaa.gov>
2025-12-23 11:31:45 -0500

    Add back email_from since it does not use ApplicationMailer defaults apparently

--- config/initializers/solid_errors.rb
+++ config/initializers/solid_errors.rb
@@ -1,3 +1,10 @@
+require "google_cloud_detector"
+
 Rails.application.config.solid_errors.send_emails = Rails.env.production?
+if GoogleCloudDetector.running_on_google_cloud?
+  Rails.application.config.solid_errors.email_from = "ncrmpdataentry@googleusercontent.com"
+else
+  Rails.application.config.solid_errors.email_from = "jeremiah.blondeau+entryapplication@noaa.gov"
+end
 Rails.application.config.solid_errors.email_to = "andy.lindeman@noaa.gov, jeremiah.blondeau@noaa.gov"
 Rails.application.config.solid_errors.destroy_after = 90.days

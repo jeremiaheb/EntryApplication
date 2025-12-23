88e2f46b6c02db1fdf6a8757c148631e60990a23
Andy Lindeman <andy.lindeman@noaa.gov>
2025-12-23 10:55:43 -0500

    Fix mailer hostnames

--- config/initializers/action_mailer.rb
+++ config/initializers/action_mailer.rb
@@ -1,10 +1,17 @@
+require "google_cloud_detector"
+
 case Rails.env
 when "development", "test"
   Rails.application.config.action_mailer.default_url_options = { host: "entryapplication.example" }
   Rails.application.config.action_mailer.perform_deliveries = false
   Rails.application.config.action_mailer.raise_delivery_errors = false
 when "production"
-  Rails.application.config.action_mailer.default_url_options = { host: "entryapplication.example" }
+  if GoogleCloudDetector.running_on_google_cloud?
+    Rails.application.config.action_mailer.default_url_options = { host: "ncrmp-data-entry.fisheries.noaa.gov" }
+  else
+    Rails.application.config.action_mailer.default_url_options = { host: "grunt.sefsc.noaa.gov" }
+  end
+
   Rails.application.config.action_mailer.delivery_method = :sendmail
   Rails.application.config.action_mailer.perform_deliveries = true
   Rails.application.config.action_mailer.raise_delivery_errors = true

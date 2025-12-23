2fa9264ae4227157149907a0960293e8d8286d63
Andy Lindeman <andy.lindeman@noaa.gov>
2025-12-23 10:51:39 -0500

    Fix default mail sender

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,11 @@
+require "google_cloud_detector"
+
 class ApplicationMailer < ActionMailer::Base
-  default from: "NCRMP Data Entry <jeremiah.blondeau+entryapplication@noaa.gov>"
+  if GoogleCloudDetector.running_on_google_cloud?
+    default from: "NCRMP Data Entry <ncrmpdataentry@googleusercontent.com>"
+  else
+    default from: "NCRMP Data Entry <jeremiah.blondeau+entryapplication@noaa.gov>"
+  end
+
   layout "mailer"
 end
